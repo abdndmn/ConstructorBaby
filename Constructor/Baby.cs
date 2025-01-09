46e05fa0080f50f9c6c633a36ec0d6c31abfe72d
abdndmn <abdndmn@hotmail.com>
2025-01-09 18:01:02 +0300

    Add project files.

---
+++ Constructor/Baby.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Constructor
+{
+    public class Baby
+    {
+        public DateTime BirthDate { get; set; }
+        public string Name { get; set; }
+        public string Surname {  get; set; }
+
+        public Baby()//Default Constructor
+        {
+            Console.WriteLine("Ingaaaa\n");
+            BirthDate = DateTime.Now;
+        }
+        public Baby(string name, string surname)// isim ve soyisim alan constructor
+        {
+            Name = name;
+            Surname = surname;
+            Console.WriteLine("\nIngaaaa\n");
+            BirthDate = DateTime.Now;
+        }
+    };
+    
+}
+
+
---
+++ Constructor/Program.cs
@@ -0,0 +1,12 @@
+﻿using Constructor;
+
+Baby baby1 = new Baby//Default contructori kullanır.
+{
+    BirthDate = DateTime.Now,
+    Name = "John",
+    Surname = "Doe"
+};
+Console.WriteLine("First Baby:\nName: "+baby1.Name+"\nSurname: "+baby1.Surname+"\nBirth Date: "+baby1.BirthDate);
+
+Baby baby2 = new Baby("Ebubekir", "Sıddık");//2. constuctori kullanır.
+Console.WriteLine("First Baby:\nName: " + baby2.Name + "\nSurname: " + baby2.Surname + "\nBirth Date: " + baby2.BirthDate);
